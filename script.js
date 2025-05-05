20f388b2f85e93e8959d8c27297afe504a6452ef
bryanT062 <bryantate899@gmail.com>
2025-05-05 12:00:49 +0200

    variable ajouté nom de la fonction ajouter au boutton

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-//Je déclare mes variable associer a mes classe HTML
+//Je déclare mes variable ARTICLE associer a mes classe HTML
 const a_eiffel = document.getElementsByClassName("p_eiffel")
 const a_dame = document.getElementsByClassName("p_notredame")
 const a_triomphe = document.getElementsByClassName("p_triomphe")
 const a_michel = document.getElementsByClassName("p_saint-michel")
 const a_versaille = document.getElementsByClassName("p_versaille")
+//Variable BOUTTON
+const btn_e = document.getElementsByClassName("btn_eiffel")
+const btn_d = document.getElementsByClassName("btn_dame")
+const btn_a = document.getElementsByClassName("btn_arc")
+const btn_m = document.getElementsByClassName("btn_michel")
+const btn_v = document.getElementsByClassName("btn_versaille")
+
+
+//fonction pour afficher mon article selon le boutton monument
+
+function appel(){
+    
+}
