3fd4342baa80b80f9a038927a29cf5ea3dfe5b44
bryanT062 <bryantate899@gmail.com>
2025-05-05 13:33:46 +0200

    création de la fonction

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 //Je déclare mes variable ARTICLE associer a mes classe HTML
-const a_eiffel = document.getElementsByClassName("p_eiffel")
-const a_dame = document.getElementsByClassName("p_notredame")
-const a_triomphe = document.getElementsByClassName("p_triomphe")
-const a_michel = document.getElementsByClassName("p_saint-michel")
-const a_versaille = document.getElementsByClassName("p_versaille")
+const a_eiffel = document.getElementsByClassName("p_eiffel");
+const a_dame = document.getElementsByClassName("p_dame");
+const a_triomphe = document.getElementsByClassName("p_arc");
+const a_michel = document.getElementsByClassName("p_michel");
+const a_versaille = document.getElementsByClassName("p_versaille");
 //Variable BOUTTON
-const btn_e = document.getElementsByClassName("btn_eiffel")
-const btn_d = document.getElementsByClassName("btn_dame")
-const btn_a = document.getElementsByClassName("btn_arc")
-const btn_m = document.getElementsByClassName("btn_michel")
-const btn_v = document.getElementsByClassName("btn_versaille")
+const btn_e = document.getElementsByClassName("click","btn_eiffel");
+const btn_d = document.getElementsByClassName("btn_dame");
+const btn_a = document.getElementsByClassName("btn_arc");
+const btn_m = document.getElementsByClassName("btn_michel");
+const btn_v = document.getElementsByClassName("btn_versaille");
 
 
 //fonction pour afficher mon article selon le boutton monument
 
-function appel(){
-    
+function appel(btn){
+    let btn 
+
+    if (btn == btn_e) {
+
+        a_eiffel.style.display = "inline-block";
+
+    }
 }
