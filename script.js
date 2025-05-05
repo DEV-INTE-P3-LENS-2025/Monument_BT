1846adeef2db71f46f606a953eeecc7f97bede81
bryanT062 <bryantate899@gmail.com>
2025-05-05 11:28:22 +0200

    début du JS

---
+++ script.js
@@ -0,0 +1,6 @@
+//Je déclare mes variable associer a mes classe HTML
+const a_eiffel = document.getElementsByClassName("p_eiffel")
+const a_dame = document.getElementsByClassName("p_notredame")
+const a_triomphe = document.getElementsByClassName("p_triomphe")
+const a_michel = document.getElementsByClassName("p_saint-michel")
+const a_versaille = document.getElementsByClassName("p_versaille")
